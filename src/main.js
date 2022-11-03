6aebd4d21ecae64599c853d94f343494c406a095
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-11-03 10:48:28 +0100

    import primevue

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+import PrimeVue from "primevue/config";
+import Dialog from "primevue/dialog";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+
+app.use(PrimeVue);
+app.component("Dialog", Dialog);
