7d518411e7ab5fcacf01a1505b548a1b31a4d3fa
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-11-04 11:49:27 +0100

    create header

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
-    <slot> </slot>
     <!-- BACKGROUND -->
     <div class="card">
       <div class="surface-500 bg-cover">
         <!-- HEADER -->
-        <div bg-indigo-400 bg-contain w-full h-7rem top-0>
-          <h1>USERS TABLE</h1>
+        <div class="sticky bg-indigo-400 bg-contain w-full h-7rem right-0">
+          <div class="text-5xl text-white font-semibold">
+            <h1>VUE APP</h1>
+          </div>
         </div>
+
+        <!-- MAIN -->
+        <div>
+          <slot> </slot>
+        </div>
+        <!-- FOOTER -->
+        <div></div>
       </div>
     </div>
   </div>
-
-  <main></main>
-  <footer></footer>
 </template>
 
 <script>
